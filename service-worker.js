3b11ed4d6f8f3e28ce55973b5bb08aac585ffbd6
Ylli Abazi <ylli.abazi019@gmail.com>
2020-05-21 22:42:46 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myLibrary-ReactJS/precache-manifest.5f96b4273383f8fe1e1258a01f072c8e.js"
+  "/yllabazi.github.io/myLibrary-ReactJS/precache-manifest.de38e9ccdaa918a75caf5dcc328e67b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myLibrary-ReactJS/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yllabazi.github.io/myLibrary-ReactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
