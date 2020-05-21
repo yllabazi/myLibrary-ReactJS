6bdf7fb1691dcf343cffba7303091f0ae6d1ac85
Ylli Abazi <ylli.abazi019@gmail.com>
2020-05-21 11:34:53 +0200

    Updates

---
+++ precache-manifest.5f96b4273383f8fe1e1258a01f072c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1402f3e54fb6128390018b76cbc32e",
+    "url": "/myLibrary-ReactJS/index.html"
+  },
+  {
+    "revision": "15511327391788d8e9b7",
+    "url": "/myLibrary-ReactJS/static/css/main.e8c7a4ae.chunk.css"
+  },
+  {
+    "revision": "24ce5e0dc8d174eefa4d",
+    "url": "/myLibrary-ReactJS/static/js/2.044530ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myLibrary-ReactJS/static/js/2.044530ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15511327391788d8e9b7",
+    "url": "/myLibrary-ReactJS/static/js/main.5c8539c7.chunk.js"
+  },
+  {
+    "revision": "8b05d49e8ea538b6332f",
+    "url": "/myLibrary-ReactJS/static/js/runtime-main.3bd31250.js"
+  }
+]);
